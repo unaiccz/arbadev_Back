b42371fec2185d8b9ae024d6b948ccab91311522
unai <unaicc2003@gmail.com>
2025-04-09 14:01:09 +0200

    fix:port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 
-const port = 3000;
+const port = process.env.PORT || 4000; // puerto donde escuchará el servidor
 app.use(cors()); // habilitar cors para permitir peticiones de otros dominios
 app.use(express.json()); // usar el middleware para parsear el body de las peticiones a json
 app.get('/', (req, res) => {
