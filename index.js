645dbbe69e8bb3d7f021f924c1b7b0e9ba52b875
unai <unaicc2003@gmail.com>
2025-04-09 13:49:27 +0200

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express'; 
+import cors from 'cors';
 const app = express();
 
 
@@ -9,4 +9,7 @@ app.use(cors()); // habilitar cors para permitir peticiones de otros dominios
 app.use(express.json()); // usar el middleware para parsear el body de las peticiones a json
 app.get('/', (req, res) => {
   res.send({ok:true, res: 'Hello World!'});
+});
+app.listen(port, () => {
+  console.log(`Escuchando en el puerto ${port}`);
 });
